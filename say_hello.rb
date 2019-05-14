3fe1eb421dea4a7138d327b374df2769bda54c9e
Joshua Wright <joshuaaaronwright@outlook.com>
2019-05-14 21:00:32 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
-def say_hello (name ,default = "Ruby Programmer")
+def say_hello (name = "Ruby Programmer")
   puts Hash.new { |hash, key| hash[key] =  }ello #{name}!
 end
 say_hello 
